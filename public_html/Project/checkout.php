50569bb693294e434a3273215df7c4481bdf3a4a
Vraj Shah <vrajs16108@gmail.com>
2021-12-12 13:08:29 -0500

    php templating for products on checkout form

--- public_html/Project/checkout.php
+++ public_html/Project/checkout.php
@@ -1,4 +1,26 @@
 <?php require(__DIR__ . "/../../partials/nav.php");
+if (is_logged_in(true) && isset($_POST["checkout"])) {
+    $userid = se(get_user_id(), null, "", false); //User id
+    $db = getDB();
+    $checkoutSumQuery = "SELECT desired_quantity, unit_price, name, description, stock FROM Cart INNER JOIN Products ON Cart.product_id = Products.id where user_id=:userid";
+    $stmt = $db->prepare($checkoutSumQuery);
+
+    try {
+        $stmt->execute([":userid" => $userid]);
+        $r = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if ($r) {
+            $checkoutSummary = $r;
+        }
+        $totalAmountOfItems = 0;
+        foreach ($checkoutSummary as $item) {
+            $totalAmountOfItems += se($item, "desired_quantity", null, false);
+        }
+    } catch (PDOException $e) {
+        flash("<pre>" . var_export($e, true) . "</pre>");
+    }
+};
+
+
 if (is_logged_in(true) && isset($_POST["checkout"])) : ?>
     <div class="container">
         <main>
@@ -10,36 +32,28 @@ if (is_logged_in(true) && isset($_POST["checkout"])) : ?>
                 <div class="col-md-5 col-lg-4 order-md-last">
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
                         <span class="text-primary">Your cart</span>
-                        <span class="badge bg-primary rounded-pill">3</span>
+                        <span class="badge bg-primary rounded-pill"><?php se($totalAmountOfItems) ?></span>
                     </h4>
                     <ul class="list-group mb-3">
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <div>
-                                <h6 class="my-0">Product name</h6>
-                                <small class="text-muted">Brief description</small>
-                            </div>
-                            <span class="text-muted">$12</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <div>
-                                <h6 class="my-0">Second product</h6>
-                                <small class="text-muted">Brief description</small>
-                            </div>
-                            <span class="text-muted">$8</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between lh-sm">
-                            <div>
-                                <h6 class="my-0">Third item</h6>
-                                <small class="text-muted">Brief description</small>
-                            </div>
-                            <span class="text-muted">$5</span>
-                        </li>
+                        <?php $amount = 0;
+                        foreach ($checkoutSummary as $item) : ?>
+                            <li class="list-group-item d-flex justify-content-between lh-sm">
+                                <div>
+                                    <h6 class="my-0"><?php se($item, "name") ?> x ( <?php se($item, "desired_quantity") ?> )</h6>
+                                    <small class="text-muted"><?php se($item, "description") ?></small>
+                                </div>
+                                <span class="text-muted"><?php se($item, "unit_price");
+                                                            $amount += se($item, "unit_price", null, false)  ?></span>
+                            </li>
+                        <?php endforeach ?>
                         <li class="list-group-item d-flex justify-content-between">
                             <span>Total (USD)</span>
-                            <strong>$20</strong>
+                            <strong><?php se($amount) ?></strong>
                         </li>
                     </ul>
                 </div>
+
+
                 <div class="col-md-7 col-lg-8">
                     <h4 class="mb-3">Billing address</h4>
                     <form method="POST" action="/Project/confirmorder.php">
